2c336449885bb26c9e408519f677dcb0acc4a1c3
JPLABRADOR <vanhagarsux@gmail.com>
2020-07-28 18:16:42 -0700

    Solo Project Test File

--- tests/fenderModShop.js
+++ tests/fenderModShop.js
@@ -1,70 +1,44 @@
 var fender = {}
-var modData = require('../testAssets/fenderModShopArray')
+var angleData = require('../testAssets/fenderModShopBassAngleArray')
+var modData1 = require('../testAssets/fenderModShopArray1')
+var modData2 = require('../testAssets/fenderModShopArray2')
 var editData = require('../testAssets/fenderModShopEditArray')
 var quantityData = require('../testAssets/fenderModShopQuantityArray')
+var bodData = require('../testAssets/fenderModShopBodyArray')
+var orienData = require('../testAssets/fenderModShopOrientationArray')
+var neckData1 = require('../testAssets/fenderModShopNeckShapeCloseWindowArray')
+var neckData2 = require('../testAssets/fenderModShopNeckShapeArray')
+var fboardData = require('../testAssets/fenderModShopFingerboardArray')
+var pickguardData = require('../testAssets/fenderModShopPickguardArray')
+var pickupsData1 = require('../testAssets/fenderModShopPickupsCloseWindowArray')
+var pickupsData2 = require('../testAssets/fenderModShopPickupsArray')
 
 module.exports = {
     beforeEach: browser => {
         fender = browser.page.fenderModShopPage()
-        fender.maximizeWindow()
-        fender.navigate()
+        fender
+            .maximizeWindow()
+            .navigate()
     },
     afterEach: browser => {
         fender.end()
     },
-
     'Test 1 - Verify Jazz Bass Mod Shop Page UI Functionality': browser => {
         fender
-            .navigateToModPage()
-            .checkBassPageFunctionality()
-            .refreshAndVerifyCartQuantity("1")
+            .verifyBassPageUI(angleData, orienData, bodData, neckData1, 
+                neckData2, fboardData, pickguardData, pickupsData1, 
+                pickupsData2)
     },
-
-    'Test 2 - Verify Shopping Cart Details and Remove From Cart Functionality': browser => {
+    'Test 2 - Verify Cart Details and Remove From Cart Functionality': browser => {
         fender
-            .navigateToModPage()
-
-        modData.forEach(test => {
-            fender
-                .bassMod(test)
-        })
-
-        fender
-            .refreshAndVerifyCartQuantity("2")
-            .navigateToShoppingCart()
-            .verify2BassShoppingCart()
-            .deleteBassShoppingCart()
-            .verifyBassShoppingCart()
-            .deleteBassShoppingCart()
-            .verifyEmptyShoppingCart()
+            .verifyCartDetailsAndRemoval(modData1, modData2)
     },
-    
     'Test 3 - Verify Cart Quantity Selector Functionality': browser => {
         fender
-            .navigateToModPage()
-            .bassMod(editData[0])
-            .refreshAndVerifyCartQuantity("1")
-            .navigateToShoppingCart()
-            .verifyBassShoppingCart()
-
-        quantityData.forEach(test => {
-        fender
-            .verifyQuantitySelector(test)
-        })
-
-        fender
-            .verifyBassShoppingCart()
+            .verifyCartQuantitySelector(editData, quantityData)
     },
-    
     'Test 4 - Verify Edit and Update Cart Functionality': browser => {
         fender
-            .navigateToModPage()
-            .bassMod(editData[0])
-            .refreshAndVerifyCartQuantity("1")
-            .navigateToShoppingCart()
-            .editBassWithReset(editData[0])
-            .editBassColor()
-            .updateCart()
+            .verifyEditAndUpdateCart(editData)
     }
-
 }
